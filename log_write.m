aa36f9dd618c6268c64d8bfafa91407daad6a5ab
lucasabdalah <lucasabdalah@gmail.com>
2022-06-08 10:46:31 -0300

    task: allow escape characteres

--- log_write.m
+++ log_write.m
@@ -1,17 +1,18 @@
-function reg_log(yourMsg)
-    %reg_log.m - Description
+function log_write(yourMsg)
+    %log_write.m - Description
     %
-    % Syntax: output = myFun(input)
+    % Syntax: log_write(yourMsg)
     %
     % Long description
     
-    fid = fopen(fullfile('YourLogFile.txt'), 'a');
+    fid = fopen(fullfile('log.txt'), 'a');
     
     if fid == -1
         error('Cannot open log file.');
     end
     
-    fprintf(fid, '%s: %s\n', datestr(now, 0), yourMsg);
+    fprintf(fid, '--------------------\n%s\n--------------------\n%s\n\n', ...
+        datestr(now, 0), sprintf(yourMsg));
     fclose(fid);
     
     end
